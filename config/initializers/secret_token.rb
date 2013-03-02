f1444944ac7afa648d8de9ad91771485535dbbaa
Dave Laird <dave@urbanspoon.com>
2013-03-02 14:24:53 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pos::Application.config.secret_token = '16f487df6e81d20f7b6564f938f18b0f81a55da5b7e9237ba613002526c8bd67e09635e943418033aae547cbb583a7c3dfc424794bc3667b759aa227e5955e37'
