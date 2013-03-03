d79cf1eb2fa01a5364faaf215b35998d734e9871
Dave Laird <dave@urbanspoon.com>
2013-03-02 16:24:49 -0800

    Added order controller

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,3 +1,9 @@
 Pos.Router.map(function() {
-
+	this.resource('orders', function() {
+    this.resource('order', {path:':order_id'});
+    this.route('new');
+  });
+	this.resource('products', function() {
+		this.resource('product', {path:'product_id)'});
+	})
 });
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,36 @@
 class OrdersController < ApplicationController
+
+  def index
+    orders = Order.all
+    render json: orders
+  end
+
+	def show
+    order = Order.find(params[:id])
+    render json: order
+  end
+
+  def create
+    order = Orders.new(params[:order])
+    if order.save
+      render json: order, status: :created
+    else
+      render json: order.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    order = Orders.find(params[:id])
+    if order.update_attributes(params[:order])
+      render json: order, status: :ok
+    else
+      render json: order.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    order = Orders.find(params[:id])
+    order.destroy
+    render json: nil, status: :ok
+  end
 end
