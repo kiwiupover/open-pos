8576cf02798b389b91facfdd88201a8205964ab2
Jonah <jonahkmoses@gmail.com>
2013-03-02 18:20:39 -0800

    update seeds, line items

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,17 @@ Product.create name: "16GB Memory", description: "Great mac compatible Ram", pri
 Product.create name: "32GB Memory", description: "Great mac compatible Ram", price_cents: 499995, upc_code: SecureRandom.hex, category_id: 2
 Product.create name: "48x DVD Drive", description: "3.5 Drive", price_cents: 16999, upc_code: SecureRandom.uuid, category_id: 3
 
-Order.create customer_email: "joe@joe.com", paid: true, subtotal_cents: 10000, tax_cents: 10000*.095, total_cents: 10000*1.095
-LineItem.create order_id: 1, product_id: 5, quantity: 2, price_cents: 10000, extended_cents: 10000*2,
-
-
-
-
+Order.create customer_email: "desiree@desiree.com", paid: true, subtotal_cents: 39900, tax_cents: 39900*.095, total_cents: 39900*1.095
+LineItem.create order_id: 1, product_id: 1, quantity: 2, price_cents: 39900, extended_cents: 39900*2,
+Order.create customer_email: "dave@dave.com", paid: true, subtotal_cents: 29900, tax_cents: 29900*.095, total_cents: 29900*1.095
+LineItem.create order_id: 2, product_id: 2, quantity: 1, price_cents: 29900, extended_cents: 29900*1,
+Order.create customer_email: "troy@troy.com", paid: true, subtotal_cents: 19900, tax_cents: 19900*.095, total_cents: 19900*1.095
+LineItem.create order_id: 3, product_id: 3, quantity: 5, price_cents: 19900, extended_cents: 19900*5,
+Order.create customer_email: "jonah@jonah.com", hold: true, subtotal_cents: 9900, tax_cents: 9900*.095, total_cents: 9900*1.095
+LineItem.create order_id: 4, product_id: 4, quantity: 3, price_cents: 9900, extended_cents: 9900*3,
+Order.create customer_email: "jayden@jayden.com", hold: true; subtotal_cents: 4900, tax_cents: 4900*.095, total_cents: 4900*1.095
+LineItem.create order_id: 5, product_id: 5, quantity: 1, price_cents: 4900, extended_cents: 4900*1,
+Order.create customer_email: "cole@cole.com", hold: true, subtotal_cents: 14900, tax_cents: 14900*.095, total_cents: 14900*1.095
+LineItem.create order_id: 6, product_id: 6, quantity: 10, price_cents: 14900, extended_cents: 14900*10,
+Order.create customer_email: "britt@britt.com", subtotal_cents: 3900, tax_cents: 3900*.095, total_cents: 3900*1.095
+LineItem.create order_id: 7, product_id: 7, quantity: 1, price_cents: 3900, extended_cents: 3900*1,
