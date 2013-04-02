b5d4dd8ae205c62fa1eff0e4ba945ec448c89ac3
Dave Laird <dave@urbanspoon.com>
2013-04-02 00:14:10 -0700

    added custom line items

--- app/assets/javascripts/controllers/order_controller.js
+++ app/assets/javascripts/controllers/order_controller.js
@@ -1,12 +1,31 @@
 Pos.OrderController = Ember.ObjectController.extend({
   needs: ['order', 'lineItems'],
-
+  customLineItem: false,
   addListItem: function(item) {
     this.get('controllers.lineItems').send('addLineItem', item);
   },
+
   removeListItem: function(item) {
     this.get('controllers.lineItems').send('removeListItem', item);
   },
+
+  setUpCustomLineItem: function() {
+    this.set('name', '');
+    this.set('price', '');
+    this.set('customLineItem', true);
+  },
+
+  submit: function() {
+    var name = this.get('name'),
+        price = this.get('price'),
+        orderId = this.get('id');
+    priceCents = this._priceInCents(price);
+    if (price)
+    this.get('lineItems').createRecord({ name: name, priceCents: priceCents, orderId: orderId });
+    this.get('store').commit();
+    this.set('customLineItem', false);
+  },
+
   tax: function() {
     return  parseInt(this.get('cents') * 0.095)
   }.property('cents'),
@@ -16,7 +35,6 @@ Pos.OrderController = Ember.ObjectController.extend({
   }.property('cents', 'tax'),
 
   payment: function() {
-    // this.store.commit();
     return this.transitionToRoute('order.payment');
   },
   tender: function() {
@@ -26,5 +44,13 @@ Pos.OrderController = Ember.ObjectController.extend({
     this.set('totalCents', this.total);
     this.store.commit();
     return this.transitionToRoute('orders.new');
+  },
+  _priceInCents: function(price) {
+    splitPrice = price.split('.');
+    if (splitPrice[1] > 0) {
+      return parseInt(splitPrice[0] * 100) + parseInt(splitPrice[1]);
+    } else {
+      return parseInt(splitPrice[0] * 100);
+    }
   }
 });
