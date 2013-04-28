5c02749c9a3cf1d26d827a518aab944241e1b5a0
Dave Laird <dave@urbanspoon.com>
2013-04-27 21:01:03 -0700

    fixed category touch events

--- app/assets/javascripts/views/category_item_view.js
+++ app/assets/javascripts/views/category_item_view.js
@@ -1,24 +1,14 @@
 Pos.CategoryItemView = Ember.View.extend({
 	tagName: 'li',
   classNames: ['category'],
-  // touchStart: function() {
-  //   this.trigger('itsClicked');
-  // },
-  // mouseDown: function() {
-  //   this.trigger('itsClicked');
-  // },
+  touchStart: function() {
+    this.trigger('changeCategory');
+  },
+  mouseDown: function() {
+    this.trigger('changeCategory');
+  },
 
-  eventManager: Ember.Object.create({
-     touchStart: function(event, view){
-      var contentId = view.content.id;
-      view.trigger('itsClicked');
-     },
-     mouseDown: function(){
-       touchStart();
-     }
-   }),
-
-  itsClicked: function(e) {
+  changeCategory: function(e) {
   	var contentId = this.content.id
   	this.get('controller').send('changeView', contentId)
   }
